bca222a9691586295055ca65eac8f4c70a1d2913
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2022-11-24 23:13:45 -0300

    inicial config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+import cors from "cors"
+import express, { json } from "express"
+import { MongoClient } from "mongodb"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+app.use(json())
+app.use(cors())
+
+app.listen(5000, console.log("Server is running..."))
