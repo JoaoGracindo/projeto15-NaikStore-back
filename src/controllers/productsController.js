56f003e6dea1e6ee0595284920289ca46832532d
EmersonMatthias <emersonsmatthias@gmail.com>
2022-11-26 14:50:09 -0300

    Versao com requisiçao de produtos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,11 +1,23 @@
 import { productsCollection } from "../database/db.js"
 
+//Registro de produtos
 export async function registerProducts(req,res){
     const product = req.product
     
     try{
         await productsCollection.insertOne(product)
         res.status(200).send("Produto cadastrado com sucesso")
+    } catch(error){
+        console.log(error)
+    }
+}
+
+//Pegar lista de produtos
+export async function getProducts(req,res){
+
+    try{
+        const products = await productsCollection.find().toArray()
+        res.send(products)
     } catch(error){
         console.log(error)
     }
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { registerProducts } from "../controllers/productsController.js";
+import { getProducts, registerProducts } from "../controllers/productsController.js";
 import { validateProducts } from "../middlewares/productsMiddlewares.js";
 
 const route = Router()
 
-
-
 route.post("/produtos", validateProducts, registerProducts)
-
+route.get("/produtos", getProducts)
 
 export default route
