cb19b868a8dd23c9cf305edd309cebd2856d8ac2
Paulo-Junio <paulofernandesbraga@gmail.com>
2022-11-27 19:36:13 -0300

    chore: adjust code for deploy on heroku

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -14,21 +14,15 @@ export async function registerProducts(req,res){
 
 //Pegar lista de produtos
 export async function getProducts(req,res){
-
-    try{
-        const products = await productsCollection.find().toArray()
-        res.send(products)
-    } catch(error){
-        console.log(error)
-    }
-}
-
-//Pegar lista de produtos por tipo
-export async function getProductsType(req,res){
     const category = req.params.category;
-
+    let products;
     try{
-        const products = await productsCollection.find({category}).toArray()
+        if(category){
+            products = await productsCollection.find({category}).toArray()
+        } else {
+            products = await productsCollection.find().toArray()
+        }
+        
         res.send(products)
     } catch(error){
         console.log(error)
