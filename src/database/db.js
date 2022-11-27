4661997530fa71e80dcb5a45c78b1e76c3695e7a
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2022-11-27 18:28:29 -0300

    merge

--- src/database/db.js
+++ src/database/db.js
@@ -2,4 +2,13 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+export const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+try{
+    const db = await mongoClient.connect()
+    const userCollection = db.collection("users");
+    const productCollection = db.collection("products")
+
+} catch(err){
+    console.log(err)
+}
